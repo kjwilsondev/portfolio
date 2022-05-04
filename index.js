ba089a117c90274f31596a9fd1488416d279d832
KJ Wilson <kjwilsondev@gmail.com>
2022-05-03 18:31:21 -0700

    added code for responsive profile photo

--- index.js
+++ index.js
@@ -16,3 +16,25 @@ AOS.init({});
 
   // }
 })();
+
+var swiper = new Swiper(".blog-slider", {
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: ".blog-slider__pagination",
+    clickable: true,
+  },
+});
+
+// $(window).scroll(function () {
+//   if ($(window).scrollTop() > 10) {
+//     $("#navBar").addClass("floatingNav");
+//   } else {
+//     $("#navBar").removeClass("floatingNav");
+//   }
+// });
